6e861b17e2527bcf383626466f64f351e0013f97
Juan Alvarez <alvarez.juanign@gmail.com>
2023-11-30 03:05:59 -0300

    add switch for select °C or °F

--- 05-weather-app/main.js
+++ 05-weather-app/main.js
@@ -4,10 +4,22 @@ const nameCity = document.querySelector('.city')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
 const pressure = document.querySelector('.pressure')
+const selectUnits = document.querySelector('#check')
 
 const API_KEY = '979046cf3730ad09da6a2e07f6b018db'
 
 const getWeather = (city) => {
+  let units = ''
+  let symbol = ''
+
+  if (selectUnits.checked) {
+    units = 'metric'
+    symbol = ' °C'
+  } else {
+    units = 'imperial'
+    symbol = ' °F'
+  }
+
   if (city === '') return
 
   if (city.length <= 3) {
@@ -27,11 +39,11 @@ const getWeather = (city) => {
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`)
       .then(res => res.json())
       .then(data => {
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=${API_KEY}`)
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${data[0].lat}&lon=${data[0].lon}&units=${units}&appid=${API_KEY}`)
           .then(response => response.json())
           .then(weather => {
             nameCity.textContent = data[0].name
-            temperature.textContent = Math.round(weather.current.temp) + ' °C'
+            temperature.textContent = Math.round(weather.current.temp) + symbol
             humidity.textContent = 'Humidity: ' + weather.current.humidity + ' %'
             pressure.textContent = 'Pressure: ' + weather.current.pressure + ' hPa'
           })
